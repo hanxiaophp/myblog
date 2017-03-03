576ca1bc5491d2b4affbd206a6c7df24fad2095b
hanxiaophp <hanxiaophp@163.com>
2017-03-03 18:03:43 +0800

    modified:   .idea/workspace.xml 	modified:   application/admin/view/login.html 	modified:   public/static/admin/css/login.css 	modified:   public/static/admin/js/login.js

--- public/static/admin/js/login.js
+++ public/static/admin/js/login.js
@@ -2,5 +2,29 @@
  * Created by hanxiao on 2017/3/3.
  */
 $(function(){
-    console.log('asdasd');
+    var $name = $('#Name');
+    var $password = $('#Password');
+    var $ename = $('.Ename');
+    var $epass = $('.Epass');
+    var result = true;
+    $('#sub').on('click', function(){
+        if ($.trim($name.val()) === '') {
+            $ename.show();
+            result = false;
+        }
+        if ($.trim($password.val()) === '') {
+            $epass.show();
+            result = false;
+        }
+        if (result === false) {
+            return false;
+        } else {
+            $.post('/admin/login', {username:$name.val(),password:$password.val()}, function(data){
+                
+            });
+        }
+    });
+    $('input').on('focus', function(){
+        $('.error').hide();
+    });
 });
--- runtime/temp/f68bebd6f33966d76b4fa050b5fe03c8.php
+++ runtime/temp/f68bebd6f33966d76b4fa050b5fe03c8.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:62:"D:\wamp\www\myblog\public/../application/admin\view\login.html";i:1488531367;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:62:"D:\wamp\www\myblog\public/../application/admin\view\login.html";i:1488534690;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,14 +10,16 @@
 <body>
     <form class="loginForm">
         <div class="form-group">
-            <label for="exampleInputEmail1">用户名：</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="username">
+            <label for="Name">用户名：</label>
+            <input type="text" class="form-control" id="Name" placeholder="username">
+            <p class="Ename error">请输入用户名</p>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">密码：</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <label for="Password">密码：</label>
+            <input type="password" class="form-control" id="Password" placeholder="Password">
+            <p class="Epass error">请输入密码</p>
         </div>
-        <button type="submit" class="btn btn-default" style="margin-left:40%;">登录</button>
+        <button type="submit" id="sub" class="btn btn-default" style="margin-left:40%;">登录</button>
     </form>
 </body>
 </html>
